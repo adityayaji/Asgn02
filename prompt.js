be06dd9cc222bf3410e0fac4bebd1a486d074b1c
aditya-yaji <adityagyaji@gmail.com>
2021-09-18 10:38:23 +0530

    main commit

--- prompt.js
+++ prompt.js
@@ -1,9 +1,9 @@
 var no ="n";
 		let form=prompt(`Did you fill the form (y/n) ?`);
-		if(form=="n")
+		if(form =="n")
 		{
 			alert(`Please fill the form!`);
 		}else
 		{
-			alert("Glad to know!")
+			alert("Glad to know!");
 		}
